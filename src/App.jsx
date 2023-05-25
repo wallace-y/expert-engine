ce3cc4a0743fae3c20d3caf6e2e359ae3703fb9f
wallace-y <cameronwfinney@gmail.com>
2023-05-25 16:26:30 +0100

    adding user ordering functionality

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,11 @@ import WelcomeMessage from "./components/WelcomeMessage";
 import ListOfItems from "./components/ListOfItems";
 import SellItems from "./components/SellItems";
 import Basket from "./components/Basket";
+import CheckoutMessage from "./components/CheckoutMessage";
 
 function App() {
   const [basketItems, setBasketItems] = useState([]);
+  const [previousOrders, setPreviousOrders] = useState([]);
 
   return (
     <>
@@ -29,8 +31,16 @@ function App() {
         <Route
           path="/basket"
           element={
-            <Basket basketItems={basketItems} setBasketItems={setBasketItems} />
+            <Basket
+              basketItems={basketItems}
+              setBasketItems={setBasketItems}
+              setPreviousOrders={setPreviousOrders}
+            />
           }
+        ></Route>
+        <Route
+          path="/ordered"
+          element={<CheckoutMessage previousOrders={previousOrders} />}
         ></Route>
       </Routes>
     </>
--- src/components/Basket.jsx
+++ src/components/Basket.jsx
@@ -1,16 +1,25 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
-function Basket({ basketItems, setBasketItems }) {
+function Basket({ basketItems, setBasketItems, setPreviousOrders }) {
   const [items, setItems] = useState([]);
+
+  let totalCost = basketItems.reduce((a, b) => {
+    return a + b.price;
+  }, 0);
+
+  let formattedTotalPrice = `£ ${totalCost / 100}`;
 
   return (
     <section>
       <h1>Here's what is in your basket</h1>
       {basketItems.map((thing, index) => {
+        let formattedPrice = thing.price / 100;
+
         return (
           <div key={index}>
             <p>{thing.item_name}</p>
-            <p>{thing.price}</p>
+            <p>£{formattedPrice}</p>
             <button
               onClick={() => {
                 setBasketItems((currentList) => {
@@ -25,8 +34,21 @@ function Basket({ basketItems, setBasketItems }) {
           </div>
         );
       })}
-      <p>Total cost £XXX</p>
-      <button>Buy now</button>
+      <p>Total cost {formattedTotalPrice}</p>
+      <Link to="/ordered">
+        <button
+          onClick={() => {
+            setPreviousOrders((currentList) => {
+              let newList = [...currentList];
+              newList.push(basketItems);
+              setBasketItems([]);
+              return newList;
+            });
+          }}
+        >
+          Buy now
+        </button>
+      </Link>
       <button>Keep shopping</button>
     </section>
   );
