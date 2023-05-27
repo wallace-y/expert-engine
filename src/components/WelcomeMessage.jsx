e842324cf777e76e2e01095c47580f96271f6bfc
wallace-y <cameronwfinney@gmail.com>
2023-05-27 14:42:59 +0100

    styling the welcome page

--- src/components/WelcomeMessage.jsx
+++ src/components/WelcomeMessage.jsx
@@ -3,9 +3,25 @@ import { Link } from "react-router-dom";
 function WelcomeMessage() {
   return (
     <section>
-      <h1>What would you like to do today?</h1>
-      <Link to="/items">Buy</Link>
-      <Link to="/sellItems">Sell</Link>
+      <div class="px-4 py-5 my-5 text-center">
+        <p>
+          <i class="fa-regular fa-trash-can fa-2xl"></i>
+        </p>
+        <h1 class="display-5 fw-bold text-body-emphasis">
+          Welcome to the Marketplace of Junk
+        </h1>
+        <div class="col-lg-6 mx-auto">
+          <p class="lead mb-4">What would you like to do today?</p>
+          <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+            <Link to="/items" className="link-dark">
+              I'm here to buy
+            </Link>
+            <Link to="/sellItems" className="link-dark">
+              I'm here to sell
+            </Link>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
