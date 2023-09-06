abc5b75202cd08cb743bdbd6a8fe5bf2b32ca5b8
wallace-y <cameronwfinney@gmail.com>
2023-09-06 19:53:46 +0100

    changng redirect message on signuo

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -18,7 +18,29 @@ function SignUp() {
   }
 
   if (loggedIn) {
-    return <h1 className="text-center mt-5">Welcome {currentUser.username}</h1>;
+    return (
+      <section>
+        <div class="px-4 py-5 my-5 text-center">
+          <p>
+            <i class="fa-regular fa-trash-can fa-2xl"></i>
+          </p>
+          <h1 class="display-5 fw-bold text-body-emphasis">
+            Welcome {username} <strong>the Marketplace</strong>
+          </h1>
+          <div class="col-lg-6 mx-auto">
+            <p class="lead mb-4">What would you like to do today?</p>
+            <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+              <Link to="/items" className="link-dark">
+                I'm here to buy
+              </Link>
+              <Link to="/sellItems" className="link-dark">
+                I'm here to sell
+              </Link>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
   }
 
   return (
